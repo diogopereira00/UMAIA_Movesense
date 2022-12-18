f3bc1ddb8cc75288df7dddea3ba7d7023a370080
Diogo Pereira <a036751@ismai.pt>
2022-12-18 19:24:36 +0000

    adicionada rota studies/user_id, fix de bugs

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -55,7 +55,6 @@ router.post("/login", (req, res, next) => {
 			});
 		}
 		if (!result.length) {
-			console.log("aqi")
 			return res.status(401).send({
 				msg: "Username or password is incorrect!",
 			});
@@ -98,6 +97,181 @@ router.post("/login", (req, res, next) => {
 		});
 	});
 });
+
+router.get('/studies/:user_id', userMiddleware.isLoggedIn, (req, res, next) => {
+	var userID = req.params.user_id;
+	console.log(userID)
+	var studie = db.query(`SELECT * FROM user_studies WHERE user_id =${db.escape(userID)}`, (err, result) => {
+		if (err) {
+			throw err;
+			return res.status(400).send({
+				msg: err,
+			});
+		}
+		if (!result.length) {
+			return res.status(401).send({
+				msg: "Não existe nenhum user com esse id",
+			});
+		}
+
+		console.log(result[0]);
+
+		return res.status(200).send({
+			studiesId: result.map(res => res.study_id),
+		});
+	})
+
+});
+//Retornatodos os estudos e dados dos estudos do user
+router.get('/studies/:user_id', userMiddleware.isLoggedIn, (req, res, next) => {
+	var userID = req.params.user_id;
+	console.log(userID)
+	var studie = db.query(`SELECT studies.id as 'studies_id', studies.name as 'studies_name', studies.description as 'studies_description',studies.start_date as 'studies_startdate', studies.end_date as 'studies_enddate', studies.version as 'studies_version',
+	surveys.id as 'surveys_id', surveys.title as 'surveys_title', surveys.description as 'surveys_description', surveys.expected_time as 'surveys_expected_time', surveys.created_at as 'surveys_created_at', surveys.updated_at as 'surveys_updated_at',
+	sections.id as 'sections_id', sections.name as 'sections_name', 
+	questions.id as 'questions_id', questions.text as 'questions_text', questions.question_type_id as 'questions_type_id', 
+	question_options.*
+	FROM user_studies
+	INNER JOIN studies ON user_studies.study_id = studies.id
+	INNER JOIN surveys ON studies.id = surveys.study_id
+	INNER JOIN sections ON surveys.id = sections.survey_id
+	INNER JOIN questions ON sections.id = questions.section_id
+	INNER JOIN question_options ON question_options.question_id = questions.id
+	WHERE user_studies.user_id = ${db.escape(userID)}
+	`, (err, result) => {
+		if (err) {
+			throw err;
+			return res.status(400).send({
+				msg: err,
+			});
+		}
+		if (!result.length) {
+			return res.status(401).send({
+				msg: "Não existe nenhum user com esse id",
+			});
+		}
+
+		console.log(result[0]);
+		// Execute the query (assuming the result is stored in a variable called "result")
+		const studies = new Map();
+		const optionsMap = new Map();
+
+		for (const row of result) {
+			console.log(row);
+			const { study_id, studies_name, studies_description, studies_startdate, studies_enddate, studies_version, surveys_id, surveys_title, surveys_description, surveys_expected_time, surveys_created_at, surveys_updated_at, name,
+				sections_id, sections_name, questions_id, questions_text, questions_type_id, start_time, end_time, version, title, expected_time, created_at, updated_at, question_id, text, question_type_id, section_id, section_name, option_id } = row;
+
+			if (!studies.has(study_id)) {
+				studies.set(study_id, {
+					study_id: study_id,
+					study_name: studies_name,
+					study_description: studies_description,
+					study_description: studies_startdate,
+					study_description: studies_enddate,
+					study_version: studies_version,
+					surveys: []
+				});
+			}
+
+			let survey = studies.get(study_id).surveys.find(survey => survey.id === surveys_id);
+			if (!survey) {
+				survey = {
+					id: surveys_id,
+					survey_title: surveys_title,
+					survey_description: surveys_description,
+					survey_expected_time: surveys_expected_time,
+					survey_created_at: surveys_created_at,
+					survey_updated_at: surveys_updated_at,
+					sections: []
+				};
+				studies.get(study_id).surveys.push(survey);
+			}
+
+			let section = survey.sections.find(section => section.id === section_id);
+			if (!section) {
+				section = {
+					section_id: sections_id,
+					section_name: sections_name,
+					questions: []
+				};
+				survey.sections.push(section);
+			}
+
+			// Check if the options map has an entry for the current question
+			if (!optionsMap.has(question_id)) {
+				optionsMap.set(question_id, new Set());
+			}
+
+			// Add the option to the set, if an option with the same id does not already exist in the set
+			const optionsSet = optionsMap.get(question_id);
+			if (!optionsSet.has(option_id)) {
+				optionsSet.add(option_id);
+			}
+
+			// Convert the set to an array and assign it to the options property of the question object
+			const options = Array.from(optionsSet).map(id => ({ option_id: id }));
+			const newQuestion = {
+				question_id: questions_id,
+				question_text: questions_text,
+				question_type_id: questions_type_id,
+				options
+			};
+
+			// Check if the question already exists in the "questions" array
+			const existingQuestion = section.questions.find(q => q.question_id === questions_id);
+			if (existingQuestion) {
+				// Update the options of the existing question
+				existingQuestion.options = options;
+			} else {
+				// Add the new question to the "questions" array
+				section.questions.push(newQuestion);
+			}
+
+		}
+
+		// Convert the map to an array and return it as a JSON object
+		res.send(Array.from(studies.values()));
+
+	})
+
+});
+
+router.get('/surveys/:user_id', userMiddleware.isLoggedIn, (req, res, next) => {
+	var userID = req.params.user_id;
+	console.log(userID)
+
+	var query = `SELECT user_surveys.user_id, studies.name, survey_id, surveys.study_id, surveys.title, surveys.description, user_surveys.start_time, user_surveys.end_time, user_surveys.isCompleted, surveys.expected_time FROM user_surveys 
+	INNER JOIN surveys on user_surveys.survey_id = surveys.id 
+	INNER JOIN studies on studies.id = surveys.study_id 
+	WHERE user_surveys.user_id =${db.escape(userID)}`
+
+	//ta a retornar user_id, name, survey_id, title, description, start_time, end_time, expected_time
+
+	var user = db.query(query, (err, result) => {
+		if (err) {
+			throw err;
+			return res.status(400).send({
+				msg: err,
+			});
+		}
+		if (!result.length) {
+			return res.status(401).send({
+				msg: "Não existe nenhum user com esse id",
+			});
+		}
+
+		console.log(result[0]);
+
+
+
+
+		return res.status(200).send({ surveys: result });
+	})
+
+});
+
+
+
 
 router.get('/secret-route', userMiddleware.isLoggedIn, (req, res, next) => {
 	console.log(req.userData);
