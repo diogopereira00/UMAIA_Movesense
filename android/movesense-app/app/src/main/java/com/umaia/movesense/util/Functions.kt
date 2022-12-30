ab91439c3839fa7c0bcb274dab7f796b0873210f
diogopereira00 <83818707+diogopereira00@users.noreply.github.com>
2022-12-30 01:38:12 +0000

    fix bug no android 13 da notificaçao

--- android/movesense-app/app/src/main/java/com/umaia/movesense/util/Functions.kt
+++ android/movesense-app/app/src/main/java/com/umaia/movesense/util/Functions.kt
@@ -1,18 +1,27 @@
 package com.umaia.movesense.util
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.umaia.movesense.util.Constants.REQUEST_FOREGROUND_SERVICE_PERMISSION
 
 object Functions {
 
 
+    fun requestNeededPermissions(context: AppCompatActivity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+           if(ContextCompat.checkSelfPermission(
+                   context,
+                   Manifest.permission.POST_NOTIFICATIONS
+               ) != PackageManager.PERMISSION_GRANTED) {
+               ActivityCompat.requestPermissions(context, arrayOf(Manifest.permission.POST_NOTIFICATIONS), Constants.REQUEST_FOREGROUND_SERVICE_PERMISSION)
+           }
+        }
 
-
-    fun requestNeededPermissions(context : AppCompatActivity) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             if (ContextCompat.checkSelfPermission(
                     context,
@@ -29,7 +38,8 @@ object Functions {
                     Manifest.permission.ACCESS_FINE_LOCATION
                 ) != PackageManager.PERMISSION_GRANTED
             ) {
-                ActivityCompat.requestPermissions(context,
+                ActivityCompat.requestPermissions(
+                    context,
                     arrayOf(
                         Manifest.permission.BLUETOOTH_SCAN,
                         Manifest.permission.BLUETOOTH_CONNECT,
@@ -38,7 +48,7 @@ object Functions {
                     ), Constants.MY_PERMISSIONS_REQUEST_LOCATION
                 )
             }
-        }else {
+        } else {
             if (ContextCompat.checkSelfPermission(
                     context,
                     Manifest.permission.ACCESS_COARSE_LOCATION
@@ -48,7 +58,8 @@ object Functions {
                     Manifest.permission.ACCESS_FINE_LOCATION
                 ) != PackageManager.PERMISSION_GRANTED
             ) {
-                ActivityCompat.requestPermissions(context,
+                ActivityCompat.requestPermissions(
+                    context,
                     arrayOf(
                         Manifest.permission.ACCESS_COARSE_LOCATION,
                         Manifest.permission.ACCESS_FINE_LOCATION
