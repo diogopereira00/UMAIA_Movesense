2f1ffa519f385b8524d32e085487840ca6510616
Diogo <a036751@umaia.pt>
2023-02-21 16:47:07 +0000

    update

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,16 @@ const express = require("express");
 const cors = require("cors");
 const router = require("./routes/router.js");
 const PORT = process.env.PORT || 3000;
+const bodyParser = require("body-parser");
 
 const app = express();
 app.use(cors());
 
-app.use(express.json({ limit: '500mb' }));
-app.use(express.urlencoded({ limit: '500mb', extended: true }));
+app.use(express.json({ limit: "1gb" }));
+app.use(express.urlencoded({ limit: "1gb", extended: true }));
+
+app.use(bodyParser.json());
+
 app.use("/api", router);
 
 app.listen(PORT, () => console.log("Server running on PORT " + PORT));
--- backend/lib/db.js
+++ backend/lib/db.js
@@ -1,12 +1,12 @@
 // lib/db.js
 
 const mysql = require("mysql");
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
 	host: "127.0.0.1",
 	user: "admin",
 	database: "movesense",
 	port: 3306,
 	password: "ismai2022",
 });
-connection.connect();
+// connection.connect();
 module.exports = connection;
